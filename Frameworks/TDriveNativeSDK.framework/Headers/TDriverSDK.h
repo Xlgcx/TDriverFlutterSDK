36eb4073319e5d3b4233054597d82fece2281426
韩占禀 <zhanbing.han@xlg.cn>
2022-09-06 16:48:51 +0800

    试驾Flutter&原生SDK更新

--- Frameworks/TDriveNativeSDK.framework/Headers/TDriverSDK.h
+++ Frameworks/TDriveNativeSDK.framework/Headers/TDriverSDK.h
@@ -7,11 +7,14 @@
 
 #import <UIKit/UIKit.h>
 
+typedef void (^TSuccessBlock) (NSDictionary * _Nullable resultDic);
+typedef void (^TFailBlock) (NSString * _Nullable error);
 typedef void (^CompletionBlock) (id _Nullable responseData);
 
 NS_ASSUME_NONNULL_BEGIN
 
 @interface TDriverSDK : NSObject
+
 /**
  被退出登录回调
  */
@@ -31,9 +34,11 @@ NS_ASSUME_NONNULL_BEGIN
 /**
  登录（同步APP登录信息）
 
- @param mobile APP登录陪驾人手机号
+ @param mobile APP登录陪驾人手机号或用户名
+ @param success 成功回调
+ @param fail 失败回调
  */
-- (void)login:(NSString *)mobile;
+- (void)login:(NSString *)mobile successBlock:(TSuccessBlock)success failBlock:(TFailBlock)fail;
 
 /**
  创建服务单（同步用户信息，同步试驾单信息）
@@ -45,13 +50,11 @@ NS_ASSUME_NONNULL_BEGIN
  */
 - (void)createOrderWithUserName:(NSString *)customerName mobile:(NSString *)mobile orderId:(NSString *)orderId carNo:(NSString *)carNo;
 
-
 /**
  结束试驾单后数据回传（惠尚宝）
  @param saleNo 销售编号
  @param success 接口调用成功的回调并返回相关数据（startTime,endTime,mileage,reportId）
  @param fail 接口调用失败的回调
-
  */
 - (void)driveOrderSaleNo:(NSString *)saleNo successBlock:(CompletionBlock)success failBlock:(dispatch_block_t)fail;
 
@@ -72,24 +75,20 @@ NS_ASSUME_NONNULL_BEGIN
  */
 - (void)jumpReportDetailPage:(NSString *)saleNo;
 
-
 /**
  上传本地所有的录音文件
  */
 - (void)uploadAllRecordFileAction;
-
 
 /**
  默认日志关闭
  */
 - (void)switchLog:(BOOL)open;
 
-
 /**
  退出登录
  */
 - (void)loginOut;
-
 
 @end
 
